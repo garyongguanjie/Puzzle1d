7815c0955cfdb24405e343b523b35ae9ebb6d9da
Joseph Chan <josephchan13@gmail.com>
2018-12-03 16:28:20 +0800

    trying to implement jump right

--- work/project.tcl
+++ work/project.tcl
@@ -1,13 +1,13 @@
-set projDir "E:/Documents/mojo/puzzleboard1d/work/planAhead"
+set projDir "E:/Downloads/Puzzle1d/work/planAhead"
 set projName "puzzleboard1d"
 set topName top
 set device xc6slx9-2tqg144
 if {[file exists "$projDir/$projName"]} { file delete -force "$projDir/$projName" }
 create_project $projName "$projDir/$projName" -part $device
 set_property design_mode RTL [get_filesets sources_1]
-set verilogSources [list "E:/Documents/mojo/puzzleboard1d/work/verilog/mojo_top_0.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/reset_conditioner_1.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/edge_detector_2.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/button_conditioner_3.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/edge_detector_2.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/button_conditioner_3.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/edge_detector_2.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/button_conditioner_3.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/edge_detector_2.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/button_conditioner_3.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/display_10.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/position_11.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/pipeline_12.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/pipeline_12.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/pipeline_12.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/pipeline_12.v" "E:/Documents/mojo/puzzleboard1d/work/verilog/counter_16.v"]
+set verilogSources [list "E:/Downloads/Puzzle1d/work/verilog/mojo_top_0.v" "E:/Downloads/Puzzle1d/work/verilog/reset_conditioner_1.v" "E:/Downloads/Puzzle1d/work/verilog/edge_detector_2.v" "E:/Downloads/Puzzle1d/work/verilog/button_conditioner_3.v" "E:/Downloads/Puzzle1d/work/verilog/edge_detector_2.v" "E:/Downloads/Puzzle1d/work/verilog/button_conditioner_3.v" "E:/Downloads/Puzzle1d/work/verilog/edge_detector_2.v" "E:/Downloads/Puzzle1d/work/verilog/button_conditioner_3.v" "E:/Downloads/Puzzle1d/work/verilog/edge_detector_2.v" "E:/Downloads/Puzzle1d/work/verilog/button_conditioner_3.v" "E:/Downloads/Puzzle1d/work/verilog/display_10.v" "E:/Downloads/Puzzle1d/work/verilog/position_11.v" "E:/Downloads/Puzzle1d/work/verilog/pipeline_12.v" "E:/Downloads/Puzzle1d/work/verilog/pipeline_12.v" "E:/Downloads/Puzzle1d/work/verilog/pipeline_12.v" "E:/Downloads/Puzzle1d/work/verilog/pipeline_12.v" "E:/Downloads/Puzzle1d/work/verilog/counter_16.v"]
 import_files -fileset [get_filesets sources_1] -force -norecurse $verilogSources
-set ucfSources [list "E:/Documents/mojo/puzzleboard1d/constraint/output_pins.ucf" "E:/Programs/Mojo\ IDE/library/components/mojo.ucf"]
+set ucfSources [list "E:/Downloads/Puzzle1d/constraint/output_pins.ucf" "E:/Programs/Mojo\ IDE/library/components/mojo.ucf"]
 import_files -fileset [get_filesets constrs_1] -force -norecurse $ucfSources
 set_property -name {steps.bitgen.args.More Options} -value {-g Binary:Yes -g Compress} -objects [get_runs impl_1]
 set_property steps.map.args.mt on [get_runs impl_1]
